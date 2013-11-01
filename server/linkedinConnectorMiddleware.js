cada383952fa72ad4f8123d0b0b55a3e9ef9a858
adebski <adebski@student.agh.edu.pl>
2013-11-01 16:49:50 +0100

    Sketch implementation of Linkedin authorization

--- server/linkedinConnectorMiddleware.js
+++ server/linkedinConnectorMiddleware.js
@@ -1,7 +1,34 @@
 'use strict';
-require('passport');
-require('passport-linkedin-oauth2');
+var passport = require('passport');
+var LinkedInStrategy = require('passport-linkedin-oauth2').Strategy;
+var config = require("./config.json");
+
+passport.use(new LinkedInStrategy({
+        clientID: config.apiKey,
+        clientSecret: config.secretKey,
+        callbackURL: "http://localhost:9000/authorize/linkedin"
+        },
+      function(accessToken, refreshToken, profile, done) {
+            // asynchronous verification, for effect...
+            process.nextTick(function () {
+              return done(null, profile);
+            });
+            }
+        ));
 
 exports.linkedinConnectorMiddleware = function(request, response, next){
-	//TODO(adebski) - create middleware for POST requests
+    if (request.url !== "/authorize/linkedin"){
+        next();
+    }
+
+    var authenticateMiddleware;
+    if (request.method === "POST"){
+        authenticateMiddleware = passport.authenticate('linkedin', { scope: ['r_basicprofile'],state: 'SOME STATE'});
+    }else{
+    	authenticateMiddleware = passport.authenticate('linkedin', {
+    		successRedirect: '/',
+    		failureRedirect: '/login'
+    	});
+    }
+    authenticateMiddleware(request, response, next);
 };
