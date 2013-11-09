457d601fc13cb073e111d40bb907a59ce44d1792
adebski <adebski@student.agh.edu.pl>
2013-11-09 21:05:56 +0100

    Added missing Acces-Control header when app main page is returned

--- server/loggedApplicationMiddleware.js
+++ server/loggedApplicationMiddleware.js
@@ -8,6 +8,7 @@ exports.loggedApplicationMiddleware = function (envPath) {
 
   function routeMiddleware(request, response, next) {
     if (util.isPathEqualTo(request, ['/app', '/app/']) && request.method === 'GET') {
+      response.setHeader('Access-Control-Allow-Origin', '*');
       util.returnFile(response, resolvedPath, 'app.html');
     } else {
       next();
